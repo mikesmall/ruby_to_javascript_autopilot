1130f4b7960533c288190c0d6e0c70ef1d9d389b
mikesmall <michael.l.a.small@gmail.com>
2017-07-05 16:45:49 -0400

    Had to write pretty much the entire program just to be able to test it

--- autopilot.js
+++ autopilot.js
@@ -1,104 +1,87 @@
-// def get_new_car
-//   {
-//     city: 'Toronto',
-//     passengers: 0,
-//     gas: 100,
-//   }
-// end
-function getNewCar() {
+function getNewCar(car) {
   city: 'Toronto',
-  passengers: 0,
-  gas: 100,
+  passengers = 0,
+  gas = 100
 }
-//------------------------------------------------------------------------------
-// def add_car(cars, new_car)
-//   cars << new_car
-//   "Adding new car to fleet. Fleet size is now #{cars.length}."
-// end
-var cars = []
-function addCar(newCar) {
+
+function addCar(newCar,getNewCar) {
   cars.push(newCar);
   console.log('Adding new car to fleet. Fleet size is now ' + cars.length + ' cars.')
 }
-//------------------------------------------------------------------------------
-// def pick_up_passenger(car)
-//   car[:passengers] += 1
-//   car[:gas] -= 10
-//   "Picked up passenger. Car now has #{car[:passengers]} passengers."
-// end
 
-//------------------------------------------------------------------------------
-// def get_destination(car)
-//   if car[:city] == 'Toronto'
-//     'Mississauga'
-//   elsif car[:city] == 'Mississauga'
-//     'London'
-//   elsif car[:city] == 'London'
-//     'Toronto'
-//   end
-// end
-//------------------------------------------------------------------------------
-// def fill_up_gas(car)
-//   old_gas = car[:gas]
-//   car[:gas] = 100
-//   "Filled up to #{ get_gas_display(car[:gas]) } on gas from #{ get_gas_display(old_gas) }."
-// end
-//------------------------------------------------------------------------------
-// def get_gas_display(gas_amount)
-//   "#{gas_amount}%"
-// end
-//------------------------------------------------------------------------------
-// def drive(car, city_distance)
-//   if car[:gas] < city_distance
-//     return fill_up_gas(car)
-//   end
-//------------------------------------------------------------------------------
-//   car[:city] = get_destination(car)
-//   car[:gas] -= city_distance
-//   "Drove to #{car[:city]}. Remaining gas: #{ get_gas_display(car[:gas]) }."
-// end
-//------------------------------------------------------------------------------
-// def drop_off_passengers(car)
-//   previous_passengers = car[:passengers]
-//   car[:passengers] = 0
-//   "Dropped off #{previous_passengers} passengers."
-// end
-//------------------------------------------------------------------------------
-// def act(car)
-//   distance_between_cities = 50
-//
-//   if (car[:gas] < 20)
-//     fill_up_gas(car)
+function pickUpPassenger(car) {
+  (car.passengers += 1);
+  (car.gas -= 10);
+  console.log('Picked up passenger. Car now has ' + car.passengers + ' passengers.')
+}
 
-//   elsif (car[:passengers] < 3)
-//     pick_up_passenger(car)
-//
-//   else
-//     if (car[:gas] < distance_between_cities)
-//       return fill_up_gas(car)
-//     end
-//     drove_to = drive(car, distance_between_cities)
-//     passengers_dropped = drop_off_passengers(car)
-//     "#{drove_to} #{passengers_dropped}"
-//   end
-// end
-//------------------------------------------------------------------------------
-// def command_fleet(cars)
-//   cars.each_with_index do |car, i|
-//     action = act car
-//     puts "Car #{i + 1}: #{action}"
-//   end
-//   puts '---'
-// end
-//------------------------------------------------------------------------------
-// def add_one_car_per_day(cars, num_days)
-//   num_days.times do
-//     new_car = get_new_car
-//     puts add_car cars, new_car
-//     command_fleet cars
-//   end
-// end
+function getDestination(car) {
+  if (car.city == 'Toronto')
+    return 'Mississauga';
+  else if (car.city == 'Mississauga')
+    return 'London';
+  else if (car.city == 'London')
+    return 'Toronto';
+}
 
-// cars = []
-// add_one_car_per_day cars, 10
-//------------------------------------------------------------------------------
+function fillUpGas(car) {
+  oldGas == car.gas;
+  car.gas == 100;
+  console.log('Filled up to ' + getGasDisplay(car.gas) + ' on gas from ' + getGasDisplay(oldGas) + '.');
+}
+
+function getGasDisplay(gasAmount) {
+  console.log(gasAmount);
+}
+
+function drive(car, cityDistance) {
+  if (car.gas < cityDistance)
+    fillUpGas(car);
+  car.city = getDestination(car);
+  car.gas -= cityDistance;
+  console.log('Drove to ' + car.city + '. Remaining gas: ' + getGasDisplay(car.gas) + '.')
+}
+
+function dropOffPassengers(car) {
+  previousPassengers == car.passengers;
+  car.passengers == 0;
+  console.log('Dropped off ' + previousPassengers + ' passengers.');
+}
+
+function act(car) {
+  var distanceBetweenCities = 50
+
+  if (car.gas < 20)
+    fillUpGas(car);
+
+  else if (car.passengers < 3)
+    pickUpPassenger(car);
+
+  else if (car.gas < distanceBetweenCities)
+    fillUpGas(car);
+
+  droveTo = drive(car, distanceBetweenCities);
+  passengersDropped = dropOffPassengers(car);
+  console.log(droveTO + passengersDropped);
+}
+
+function commandFleet(cars) {
+  for (i=0; i <= cars.length; i++) {
+    var action = act(car);
+    console.log('Car ' + (i + 1) + ': ' + action);
+  }
+  console.log('---')
+}
+
+function addOneCarPerDay(cars, numDays) {
+  for (i=0; i <= numDays; i++) {
+    newCar = getNewCar;
+    console.log(addCar);
+    console.log(cars);
+    console.log(newCar);
+    commandFleet(cars);
+  }
+}
+
+var cars = []
+addOneCarPerDay(cars, 10)
