3d978133e26425a673846772133ea3254ea916a3
mikesmall <michael.l.a.small@gmail.com>
2017-07-05 17:16:30 -0400

    Corrected my syntax errors by comparing w/ @vwolanyk's submission. Working now

--- autopilot.js
+++ autopilot.js
@@ -1,18 +1,20 @@
-function getNewCar(car) {
-  city: 'Toronto',
-  passengers = 0,
-  gas = 100
+function getNewCar() {
+  return {
+    city: 'Toronto',
+    passengers: 0,
+    gas: 100
+  }
 }
 
-function addCar(newCar,getNewCar) {
+function addCar(newCar, NewCar) {
   cars.push(newCar);
-  console.log('Adding new car to fleet. Fleet size is now ' + cars.length + ' cars.')
+  return 'Adding new car to fleet. Fleet size is now ' + cars.length + ' cars.';
 }
 
 function pickUpPassenger(car) {
   (car.passengers += 1);
   (car.gas -= 10);
-  console.log('Picked up passenger. Car now has ' + car.passengers + ' passengers.')
+  return 'Picked up passenger. Car now has ' + car.passengers + ' passengers.';
 }
 
 function getDestination(car) {
@@ -30,61 +32,70 @@ function getDestination(car) {
 function fillUpGas(car) {
   oldGas == car.gas;
   car.gas == 100;
-  console.log('Filled up to ' + getGasDisplay(car.gas) + ' on gas from ' + getGasDisplay(oldGas) + '.');
+  return 'Filled up to ' + getGasDisplay(car.gas) + ' on gas from ' + getGasDisplay(oldGas) + '.';
 }
 
 function getGasDisplay(gasAmount) {
-  console.log(gasAmount);
+  return gasAmount;
 }
 
 function drive(car, cityDistance) {
   if (car.gas < cityDistance) {
-    fillUpGas(car);
+    return fillUpGas(car);
   }
   car.city = getDestination(car);
   car.gas -= cityDistance;
-  console.log('Drove to ' + car.city + '. Remaining gas: ' + getGasDisplay(car.gas) + '.')
+  return 'Drove to ' + car.city + '. Remaining gas: ' + getGasDisplay(car.gas) + '.';
 }
 
 function dropOffPassengers(car) {
-  var previousPassengers = car.passengers;
-  car.passengers == 0;
-  console.log('Dropped off ' + previousPassengers + ' passengers.');
+  previousPassengers = car.passengers;
+  car.passengers = 0;
+  return 'Dropped off ' + previousPassengers + ' passengers.';
 }
 
 function act(car) {
   var distanceBetweenCities = 50
 
   if (car.gas < 20) {
-    fillUpGas(car);
+    return fillUpGas(car);
   }
   else if (car.passengers < 3) {
-    pickUpPassenger(car);
+    return pickUpPassenger(car);
   }
-  else if (car.gas < distanceBetweenCities) {
-    fillUpGas(car);
+  else {
+    if (car.gas < distanceBetweenCities) {
+      fillUpGas(car);
+    }
+    droveTo = drive(car, distanceBetweenCities);
+    passengersDropped = dropOffPassengers(car);
+    return droveTo + " " + passengersDropped;
   }
-
-  droveTo = drive(car, distanceBetweenCities);
-  passengersDropped = dropOffPassengers(car);
-  console.log(droveTo + passengersDropped);
 }
 
 function commandFleet(cars) {
   for (i=0; i < cars.length; i++) {
-    var action = act(cars);
+    action = act(cars[i]);
     console.log('Car ' + (i + 1) + ': ' + action);
   }
   console.log('---')
 }
 
-function addOneCarPerDay(cars, numDays) {
-  for (i=0; i < numDays; i++) {
-    newCar = getNewCar;
-    console.log(addCar);
-    console.log(cars);
-    console.log(newCar);
-    commandFleet(cars);
+// function addOneCarPerDay(cars, numDays) {
+//   for (i=0; i < numDays; i++) {
+//     newCar = getNewCar;
+//     console.log(addCar);
+//     console.log(cars);
+//     console.log(newCar);
+//     commandFleet(cars);
+//   }
+// }
+
+function addOneCarPerDay(cars,numDays) {
+  for (var i = numDays; i > 0; i--) {
+    newCar = getNewCar();
+    console.log(addCar(cars,newCar));
+    commandFleet(cars)
   }
 }
 
